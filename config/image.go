6b17b875183a9275ff1389babe8583f58cc2f7c2
Rohan Kumar <rohan.kumar@deepsource.io>
2023-07-28 17:42:44 +0530

    fix: move image registry config to env

--- config/image.go
+++ config/image.go
@@ -11,25 +11,16 @@ type ImageRegistry struct {
 }
 
 func (i *ImageRegistry) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	type T struct {
-		PullSecretName string `yaml:"pullSecretName"`
-		RegistryUrl    string `yaml:"registryUrl"`
-	}
-	var v T
-	if err := unmarshal(&v); err != nil {
-		return err
-	}
 	if os.Getenv("TASK_IMAGE_PULL_SECRET_NAME") != "" {
 		i.PullSecretName = os.Getenv("TASK_IMAGE_PULL_SECRET_NAME")
-	} else {
-		i.PullSecretName = v.PullSecretName
 	}
 
-	imageURL, err := url.Parse(v.RegistryUrl)
-	if err != nil {
-		return err
+	if os.Getenv("TASK_IMAGE_REGISTRY_URL") != "" {
+		imageURL, err := url.Parse(os.Getenv("TASK_IMAGE_REGISTRY_URL"))
+		if err != nil {
+			return err
+		}
+		i.RegistryUrl = *imageURL
 	}
-
-	i.RegistryUrl = *imageURL
 	return nil
 }
