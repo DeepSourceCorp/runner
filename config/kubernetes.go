f9bbe8a3f9141de59190118958f0c0d67606e761
Rohan Kumar <rohan.kumar@deepsource.io>
2023-07-28 16:49:09 +0530

    fix: move namespace and nodeSelector config to env

--- config/kubernetes.go
+++ config/kubernetes.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Kubernetes struct {
 	Namespace     string            `yaml:"namespace"`
 	NodeSelector  map[string]string `yaml:"nodeSelector"`
@@ -16,12 +22,21 @@ func (k *Kubernetes) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	if err := unmarshal(&v); err != nil {
 		return err
 	}
-	if v.Namespace == "" {
-		v.Namespace = "default"
+	if os.Getenv("TASK_NAMESPACE") != "" {
+		k.Namespace = os.Getenv("TASK_NAMESPACE")
+	} else {
+		k.Namespace = v.Namespace
 	}
-
-	k.Namespace = v.Namespace
-	k.NodeSelector = v.NodeSelector
+	if os.Getenv("TASK_NODE_SELECTOR") != "" {
+		ns := os.Getenv("TASK_NODE_SELECTOR")
+		k.NodeSelector = make(map[string]string)
+		err := yaml.Unmarshal([]byte(ns), &k.NodeSelector)
+		if err != nil {
+			return err
+		}
+	} else {
+		k.NodeSelector = v.NodeSelector
+	}
 	k.ImageRegistry = v.ImageRegistry
 	return nil
 }
