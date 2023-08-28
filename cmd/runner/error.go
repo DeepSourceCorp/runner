a5d1b52077d6ad4c23fbdff9f1d3120d24411174
Rohan Kumar <rohan.kumar@deepsource.io>
2023-08-28 17:32:57 +0530

    fix: ignore exceptions in sentry for http code < 500

--- cmd/runner/error.go
+++ cmd/runner/error.go
@@ -9,11 +9,19 @@ import (
 )
 
 func RunnerHTTPErrorHandler(err error, c echo.Context) {
-	sentry.CaptureException(err)
 	switch typedErr := err.(type) {
 	case *httperror.Error:
+		sentry.CaptureException(err)
+		_ = c.JSON(typedErr.Code, typedErr)
+	case *echo.HTTPError:
+		if typedErr.Code < 500 {
+			_ = c.JSON(typedErr.Code, typedErr.Message)
+			return
+		}
+		sentry.CaptureException(err)
 		_ = c.JSON(typedErr.Code, typedErr)
 	default:
+		sentry.CaptureException(err)
 		_ = c.JSON(http.StatusInternalServerError, httperror.ErrUnknown(err))
 	}
 }
