e835348c81f0afdeba7080f28869526cbbb35fd5
Vishnu Jayadevan <vishnu@deepsource.io>
2023-10-12 11:39:26 +0530

    chore: add logs

--- cmd/runner/error.go
+++ cmd/runner/error.go
@@ -6,11 +6,13 @@ import (
 	"github.com/deepsourcecorp/runner/httperror"
 	"github.com/getsentry/sentry-go"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/exp/slog"
 )
 
 func RunnerHTTPErrorHandler(err error, c echo.Context) {
 	switch typedErr := err.(type) {
 	case *httperror.Error:
+		slog.Error("http error", slog.Any("err", typedErr.Unwrap()))
 		sentry.CaptureException(err)
 		_ = c.JSON(typedErr.Code, typedErr)
 	case *echo.HTTPError:
